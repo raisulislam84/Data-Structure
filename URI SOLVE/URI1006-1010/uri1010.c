c7ec0161731422672a957b7f08a076fb43626a1a
R.I.Niloy <42338483+raisulislam84@users.noreply.github.com>
2018-11-05 02:03:58 +0600

    Add files via upload

---
+++ URI SOLVE/URI1006-1010/uri1010.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int code1,unit1,code2,unit2;
+    float price1,price2,total_price;
+    scanf("%d %d %f",&code1, &unit1, &price1);
+    scanf("%d %d %f",&code2, &unit2, &price2);
+
+    total_price=(unit1*price1)+(unit2*price2);
+    printf("VALOR A PAGER: R$ %.2f\n",total_price);
+    return 0;
+}
